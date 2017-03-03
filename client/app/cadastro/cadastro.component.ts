63f42b64548779be16d5349a995efa437de3746d
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-03-03 09:15:31 -0300

    Alteração de fotos

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FotoService } from '../foto/foto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -16,9 +17,28 @@ export class CadastroComponent {
     foto: FotoComponent = new FotoComponent();
     service: FotoService;
     meuForm: FormGroup;
+    route: ActivatedRoute;
+    mensagem: string = '';
+    router: Router;
 
-    constructor(service: FotoService, fb: FormBuilder){
+    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute, router: Router){
         this.service = service;
+        this.route = route;
+        this.router = router;
+
+        this.route.params.subscribe(params => {
+
+            let id = params['id'];
+
+            if(id) {
+
+                this.service.buscaPorId(id)
+                    .subscribe(
+                        foto => this.foto = foto,
+                        erro => console.log(erro));
+            }
+         });
+
 
         this.meuForm = fb.group({
             titulo: ['', Validators.compose(
@@ -29,17 +49,16 @@ export class CadastroComponent {
         });
     }
 
-    cadastrar(event: Event) {
+    cadastrar(event) {
         event.preventDefault();
         console.log(this.foto);
 
         this.service.cadastra(this.foto)
-                    .subscribe( () => {
-                        this.foto = new FotoComponent();
-                        console.log('Foto salva com sucesso');
-
-                    }, erro => {
-                        console.log(erro);
-                    });
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                this.router.navigate(['']);
+            }, erro => {
+                console.log(erro);
+            });
     }
 }
